a96420026febb58898c9476b480a7ed140c1be77
rosemulazada <rosemulazada16@gmail.com>
2024-05-08 11:39:12 +0200

    Deploy test

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -8,31 +8,38 @@ const test = document.querySelector(".test");
 const a = document.querySelectorAll("nav ul:first-of-type li p a");
 console.log(a);
 
+// Function to handle page navigation animation
+const navigateWithLoader = (href) => {
+    if (loaderContainer) {
+        gsap.set(loaderContainer, { display: "flex" });
+        const tl = gsap.timeline();
+
+        tl.to(loaderItems, {
+            height: "100%",
+            duration: 0.45,
+            stagger: 0.22,
+            onComplete: () => {
+                // After animation completion, navigate to the new page
+                window.location.href = href;
+            },
+        });
+    }
+};
+
+// Event listener for link clicks
 a.forEach((aa) => {
     aa.addEventListener("click", (e) => {
         e.preventDefault();
-
-        if (loaderContainer) {
-            gsap.set(loaderContainer, { display: "flex" });
-            const tl = gsap.timeline();
-
-            tl.to(loaderItems, {
-                height: "100%",
-                duration: 0.45,
-                stagger: 0.22,
-                onComplete: () => {
-                    window.location.href = aa.href;
-                },
-            });
-        }
+        navigateWithLoader(aa.href); // Call the navigation function
     });
 });
 
-window.addEventListener("load", () => {
+document.addEventListener("DOMContentLoaded", () => {
     if (test) {
-        console.log(test)
-        gsap.set(loaderContainer, { display: "flex" });
+        console.log(test);
         gsap.set(loaderItems, { height: "100%" });
+
+        gsap.set(loaderContainer, { display: "flex" });
 
         const tl = gsap.timeline();
 
